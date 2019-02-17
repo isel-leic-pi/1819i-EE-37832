6eb144c552ba339363d253a3f68e589559986748
André Mauro <andrexzibit@gmail.com>
2019-02-17 23:50:33 +0000

    ..

---
+++ app/js/groups.js
@@ -0,0 +1,199 @@
+'use strict'
+
+const util = require('./util.js')
+const Handlebars = require('./../../node_modules/handlebars/dist/handlebars.js')
+const groupsHBS = require('./../views/groups.hbs')
+const groupsHTML = require('./../views/groups.html')
+
+module.exports =  async (divMain) => {
+	 
+	 try{
+        const session = await util.fetchJSON('/foca/auth/session')
+        if(!session.auth){
+			divMain.innerHTML = ''
+			return util.showAlert('You cannot access FocaGroups! Only for authenticated users.')
+			}
+    }
+    catch(err){
+        util.showAlert(JSON.stringify(err))
+    }
+
+	
+    divMain.innerHTML = groupsHTML
+	document //Search function eventListener
+		.getElementById('buttonSearch')
+		.addEventListener('click', searchHandler)
+	document //createGroup function eventListener
+		.getElementById('buttonCreateGroup')
+		.addEventListener('click', createGroupHandler)
+	
+	document //editGroup function eventListener
+		.getElementById('buttonEditGroup')
+		.addEventListener('click', editGroupHandler)	
+
+	document //copyGroup function eventListener
+    .getElementById('buttonCopyGroup')
+    .addEventListener('click', copyGroupHandler)
+
+	
+	document //insertTeam function eventListener
+		.getElementById('buttonInsertTeam')
+		.addEventListener('click', insertTeamHandler)	
+
+	document //removeTeam function eventListener
+		.getElementById('buttonRemoveTeam')
+		.addEventListener('click',removeTeamHandler)	
+	
+	const inputGroupId = document.getElementById('groupId')
+	const inputCompetitionId = document.getElementById('competitionId')
+	const inputTeamId= document.getElementById('teamId')
+	const inputGroupName = document.getElementById('groupName')
+	const inputDescriptionId = document.getElementById('descriptionId')
+
+	const divSearchResults = document.getElementById('divSearchResults')
+	const searchResultsView = Handlebars.compile(groupsHBS)
+
+
+	function createGroupHandler(ev){
+		ev.preventDefault()
+		if(!inputGroupName.value ||  !inputDescriptionId.value){
+			util.showAlert('Preencha os campos Name e Description')
+		}
+		else{
+			fetch(`http://localhost:3000/foca/groups?name=${inputGroupName.value}&description=${inputDescriptionId.value}`, {method: 'POST'})
+			.then(res => {
+				return (res.status == 409)? util.showAlert('O grupo que esta a tentar criar já existe!')
+				 : res.json()
+			})
+			.then(obj => {
+				fetch(`http://localhost:3000/foca/groups/${obj.id}`)
+				.then(ress=> ress.json())
+				.then( objj => divSearchResults.innerHTML = showGroupInfo(objj)
+				//showAlert sucesso
+				)})
+			.catch()
+		}
+	}
+
+	function editGroupHandler(ev){
+		ev.preventDefault()
+		if(!inputGroupId.value || !inputGroupName.value ||  !inputDescriptionId.value){
+			util.showAlert('Preencha os campos groupId, name e Description')
+		}
+		else{
+			fetch(`http://localhost:3000/foca/groups/${inputGroupId.value}?name=${inputGroupName.value}&description=${inputDescriptionId.value}`, {method: 'PUT'})
+			.then(res => {
+				return (res.status == 409)? util.showAlert('Já existem um grupo com este nome!')
+				 : res.json()
+			})
+			.then(obj => {
+				fetch(`http://localhost:3000/foca/groups/${obj.id}`)
+				.then(ress=> ress.json())
+				.then( objj => {
+					if(objj.teams == null) util.showAlert('não existe o grupo especificado')
+					divSearchResults.innerHTML = showGroupInfo(objj)
+				}
+				)})
+			.catch()
+		}
+	}
+
+	/**
+	 * Possibilidade de criar uma cópia de um grupo existente, incluindo todas as equipas que fazem parte desse grupo.
+	 */
+	function copyGroupHandler(ev){
+		ev.preventDefault()
+		const groupId = inputGroupId.value
+		if(groupId == ""){
+			util.showAlert('Insira o id do grupo que quer copiar')
+		}
+		else{
+			fetch(`http://localhost:3000/foca/copy/groups/${groupId}`)
+			.then(res =>{
+				res.json()
+			} )
+			.then(obj => {
+				alert(obj)
+				fetch(`http://localhost:3000/foca/groups/${obj.id}`)
+				.then(ress=> ress.json())
+				.then( objj => divSearchResults.innerHTML = showGroupInfo(objj)
+				//showAlert sucesso
+				)})
+			.catch()
+		}
+		
+	}
+
+	function insertTeamHandler(ev){
+		ev.preventDefault()
+		if(!inputGroupId.value || !inputTeamId.value || !competitionId.value){
+			util.showAlert('por favor introduza os parametros necessários')
+		}
+		else{
+			fetch(`http://localhost:3000/foca/groups/${inputGroupId.value}/competition/${competitionId.value}/team/${inputTeamId.value}`, {method: 'POST'})
+			.then(res => {
+				return (res.status == 409)? util.showAlert('Esta equipa já se encontra no grupo!')
+				 : res.json()
+			})
+				.then(obj => {
+					fetch(`http://localhost:3000/foca/groups/${obj.id}`)
+					.then(ress=> ress.json())
+					.then( objj => {
+						if(objj.teams == null) util.showAlert('não existe o grupo especificado')
+						divSearchResults.innerHTML = showGroupInfo(objj)
+					}
+					)})
+				.catch()
+		}
+	}
+	function removeTeamHandler(ev){
+		ev.preventDefault()
+		if(!inputGroupId.value || !inputTeamId.value){
+			util.showAlert('por favor introduza os parametros necessários')
+		}
+		else{
+			fetch(`http://localhost:3000/foca/groups/${inputGroupId.value}/team/${inputTeamId.value}`, {method: 'DELETE'})
+				.then(() => document.getElementById('buttonSearch').click())
+				.catch()
+		}
+	}
+
+	function searchHandler(ev){
+		ev.preventDefault()
+	 
+		if(!inputGroupId.value){
+		fetch(`http://localhost:3000/foca/groups`)
+			.then(res=>res.json())
+			.then(obj => divSearchResults.innerHTML = showAllGroupInfo(obj.groups))
+			//	alert(JSON.stringify(obj.groups))
+			.catch(err => console.log(err))
+		//util.showAlert('por favor introduza um identificador do grupo')
+		}
+		else{
+
+		const groupId = inputGroupId.value
+		fetch(`http://localhost:3000/foca/groups/${groupId}`)
+			.then(res => res.json())
+			.then(obj => {
+				if(obj.teams == null) util.showAlert('não existe o grupo especificado')
+				divSearchResults.innerHTML = showGroupInfo(obj)
+			})
+			.catch(err => console.log(err))
+		}
+	}
+	
+ 
+   function showAllGroupInfo(groups){
+	   const headers = '<table class="table"><thead><tr><th>Id</th><th>Nome</th><th>Descrição</th><th>Equipas</th></tr></thead><tbody>'
+	   let data = ''
+	   groups.forEach(element => {
+		data+=searchResultsView({element})
+	})
+	   return headers + data + '</tbody></table>'
+   }
+
+   function showGroupInfo(group){
+	const headers = '<table class="table"><thead><tr><th>Id</th><th>Nome</th><th>Descrição</th><th>Equipas</th></tr></thead><tbody>'
+	return headers + searchResultsView({group}) + '</tbody></table>'
+   }
+}
---
+++ foca-services.js
@@ -0,0 +1,181 @@
+'use strict'
+
+const FocaDB = require('./foca-db.js')
+const FootballData = require('./football-data')
+const rp = require('request-promise')
+
+class Foca {
+    constructor(es) {
+        this.focadb = FocaDB.init(es)
+        this.footballData = FootballData.init(es)
+    }
+    static init(es) {
+        return new Foca(es)
+    }
+    getCompetitions() {
+
+        return this
+            .footballData
+            .getCompetitions()
+            .catch(err => Promise.reject({code: 404}))
+
+        }
+    getTeamsByCompetitionId(id) {
+        return this
+            .footballData
+            .getTeamsByCompetitionId(id)
+            .catch(err => Promise.reject({code: 404}))
+        }
+    getGroups(user_id) {
+
+        return this
+            .focadb
+            .getGroups(user_id)
+    }
+
+    /**(1)
+	 * Não podem existir grupos com o mesmo nome, pertencente a um utilizador, 
+	 * mas podem existir grupos com o mesmo nome pertencentes a utilizadores diferentes.
+	 * Nesta implementação cada user apenás vê e edita os seus grupos.
+	 * @param {*} user_id
+	 * @param {*} name
+	 * @param {*} description
+	 */
+     createGroup(user_id, name, description) {
+        //verify if group already exist, should return error 409
+		
+		return this.focadb.getGroups(user_id)
+		.then( body=> {
+			let found =false
+			body.groups.forEach(group => {
+				if (group.name == name) {
+					found=true
+				}
+			})
+
+			return (!found)? this
+            .focadb.CreateGroup(user_id, name, description)
+			.catch(err => Promise.reject({statusCode: 404}))
+			: Promise.reject({statusCode:409})
+
+		})
+		
+        // if it doesn't find a group with the name passed as a parameter then returns
+        // the created group
+        /*return this
+            .focadb.CreateGroup(user_id, name, description)
+            .catch(err => Promise.reject({code: 404}))*/
+        }
+    getGroupById(user_id, id) {
+        return this
+            .focadb
+            .getGroupById(user_id, id)
+            .catch(err => Promise.reject({code: 404, err: err}))
+		}
+		
+    editGroup(user_id, id, name, description) {
+		return this.focadb.getGroups(user_id)
+		.then( body=> {
+			let found =false
+			body.groups.forEach(group => {
+				if (group.name == name) {
+					found=true
+				}
+			})
+
+			return (!found)? 
+			this.focadb.editGroup(user_id, id, name, description)
+			.catch(err => Promise.reject({code: 404}))
+			:Promise.reject({statusCode:409})
+			
+		})
+	}
+
+    /** (2)
+	 * Um grupo não pode ter a mesma equipa mais que uma vez.
+	 *
+	 * @param {*} user_id
+	 * @param {*} groupId
+	 * @param {*} competitionId
+	 * @param {*} teamId
+	 */
+	insertTeamInGroup(user_id,groupId, competitionId, teamId){
+
+		return this.focadb.getGroupById(user_id, groupId)
+				.then(group => { 
+					
+					let sameteam = false
+					group.teams.forEach(team =>
+						{
+							if( team.id == teamId){
+								sameteam = true
+							}
+						}
+					)
+					return (!sameteam)
+						? this.footballData.getTeamsByCompetitionId(competitionId)
+							.then(body =>{
+								let team = checkid (body.teams, teamId)
+								return this.focadb.insertGroupTeam(user_id,groupId, team)
+							})
+							.catch(err=>
+								Promise.reject({code:404}
+							))
+						: Promise.reject({statusCode: 409}) //TODO show this error msg and dont call getGroupByID
+				})
+
+	}  
+		
+
+    deleteGroupTeam(user_id, groupId, teamId) {
+        return this
+            .focadb
+            .deleteGroupTeam(user_id, groupId, teamId)
+            .catch(err => Promise.reject({code: 404}))
+
+        }
+
+    /**(3)
+	 * * Possibilidade de criar uma cópia de um grupo existente, incluindo todas as equipas que fazem parte desse grupo.
+	 */
+	 copyGroup(user_id, groupID){
+		return this.focadb.getGroupById(user_id, groupId)
+					.then(group => {
+						console.log('Copiou')
+						console.log(group)
+						return this.focadb.createGroup( user_id, group.name, group.description, group.teams) 
+					})
+		
+
+	}
+
+    getAllGamesBetweenTwoDates(user_id, groupId, dateFrom, dateTo) {
+
+        return this
+            .focadb
+            .getGroupById(user_id, groupId)
+            .then(
+                body => Promise.all(body.teams.map(team => this.footballData.getgamesfromteam(team.id, dateFrom, dateTo)))
+            )
+            .then(
+                matches => matches.reduce((accumulator, currentvalue) => accumulator.concat(currentvalue)).sort((matchesX, matchesY) => (new Date(matchesX.date) - new Date(matchesY.date)))
+            )
+            .catch(err => Promise.reject({code: 404}))
+
+        }
+
+}
+
+function checkid(source, teamid) {
+
+    let teamA
+    source.forEach(team => {
+        if (team.id == teamid) {
+            teamA = team
+        }
+    })
+    return teamA
+
+}
+
+module.exports = Foca
